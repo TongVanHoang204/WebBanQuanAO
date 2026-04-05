84f89ac788de9a91d450534a56b23be705a138a7
hoang <tongvanhoang782004@gmail.com>
2026-04-05 14:27:15 +0700

    chore: stop tracking generated dependencies

--- backend/dist/src/controllers/product.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const getProducts: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare const getProductBySlug: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare const getProductById: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare const getNewArrivals: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare const searchProducts: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const getOptions: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-//# sourceMappingURL=product.controller.d.ts.map
--- backend/dist/src/controllers/upload.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import multer from 'multer';
-export declare const upload: multer.Multer;
-export declare const uploadSingle: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const uploadMultiple: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const deleteFile: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=upload.controller.d.ts.map
